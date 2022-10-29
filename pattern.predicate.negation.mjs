04f9e58ccc59e37e05ab70ccea0df601a235d654
Lucian Hanga <lucianhanga@gmx.net>
2022-10-29 15:52:57 +0200

    add to git the negate predicate pattern

--- pattern.predicate.negation.mjs
+++ pattern.predicate.negation.mjs
@@ -1,5 +1,6 @@
- 
+import { filter } from "ramda"
 
+ 
 function negate(predicate) {
     return function() {
         return !predicate.apply(null, arguments)
@@ -7,18 +8,33 @@ function negate(predicate) {
 }
 
 
-const isEven = x => x%2 == 0
-const isOdd = negate(isEven)
-
-console.log(`3 is even: ${isEven(3)}`)
-console.log(`3 is even: ${isOdd(3)}`)
-
 const isNull = x => x === null
 const isNotNull = negate(isNull)
+const isPower = x =>Math.sqrt(x) === Math.floor(Math.sqrt(x))
 
-const  x = null
+//
+// example of predicte and negation of predicate usage together 
+// with currying
+//
 
-console.log(`x is null: ${isNull(x)}`)
-console.log(`x is not null: ${isNotNull(x)}`)
+const lst = [1,2,3,4,null,5,6,7,8,9,10,11,12,null,13]
 
+const clean_lst = lst.filter(negate(isNull))
 
+const clean_lst2 = filter(negate(isNull), lst)
+// curry
+const filterOutNull = filter(negate(isNull))
+
+const clean_lst3 = filterOutNull(lst)
+
+console.log(clean_lst)
+console.log(clean_lst2)
+console.log(clean_lst3)
+
+// curry
+const filterPowers = filter(isPower)
+
+const powerLst = filter(isPower, filter (isNotNull, lst))
+console.log( powerLst)
+const powerLst3 = filterPowers(filterOutNull(lst))
+console.log( powerLst)
